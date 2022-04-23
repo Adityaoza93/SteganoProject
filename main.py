99d08fe94697dd902a73f5e2d23c5593172f98ec
vibodhbhosure <vibodhbhoure@gmail.com>
2022-04-23 23:43:30 +0530

    Design Upgrade

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import streamlit as st
 from stegano import exifHeader as stg
-from PIL import Image
 
 def Encode(img, text):
     global fileopen
@@ -18,28 +17,37 @@ def Decode(dimg):
         f.write(dimg.getbuffer())
     dfileopen = "" + dimg.name
     text = stg.reveal(dfileopen)
-    st.write(text)
+    st.header('Decoded Text:')
+    st.subheader(text)
 
 st.title("Image Steganography")
-imag = st.radio('Select the type of file', ('Image', 'Audio', 'Video'))
+st.header("")
+st.header("")
+st.subheader('Select the type of file:')
+imag = st.radio('', ('Image', 'Audio', 'Video'))
 if imag == 'Image':
-    imgencd = st.radio('Type of Operation', ('Encode', 'Decode'))
+    st.subheader('Type of operation:')
+    imgencd = st.radio('', ('Encode', 'Decode'))
     if imgencd == 'Encode':
-        filebyte = st.file_uploader("Upload Image", accept_multiple_files=False, type='jpg')
+        st.subheader('Upload Image as JPG:')
+        filebyte = st.file_uploader("", accept_multiple_files=False, type='jpg')
         if filebyte is not None:
             st.image(filebyte)
-            opt = st.radio("Text or Text File:", ('Text', 'Text File'))
+            st.subheader('Text or Text File:')
+            opt = st.radio("", ('Text', 'Text File'))
             if opt == 'Text':
-                txt_input = st.text_input("Enter Text:")
+                st.subheader('Enter Text:')
+                txt_input = st.text_input("")
                 if txt_input and st.button("Encode") is not None: Encode(filebyte, txt_input)
             elif opt == 'Text File':
-                txt_file = st.file_uploader("Upload Text File:", accept_multiple_files=False, type='txt')
+                st.subheader('Upload File as TXT:')
+                txt_file = st.file_uploader("", accept_multiple_files=False, type='txt')
                 if txt_file is not None:
                     with open(os.path.join("", txt_file.name), "wb") as f:
                         f.write(txt_file.getbuffer())
                     with open(txt_file.name) as f:
                         data = f.readline()
-                        if data and st.button("Encode") is not None: Encode(filebyte, data)
+                        if data and st.button("Encode") is not None:Encode(filebyte, data)
     elif imgencd == 'Decode':
         dimg = st.file_uploader("Upload File", type='jpg')
         if dimg is not None:
