24bf5362543b897b1fa4fc7e5f630c0d4bc70bb7
vibodhbhosure <vibodhbhoure@gmail.com>
2022-04-23 23:26:55 +0530

    Text File Option Added

--- main.py
+++ main.py
@@ -25,11 +25,21 @@ imag = st.radio('Select the type of file', ('Image', 'Audio', 'Video'))
 if imag == 'Image':
     imgencd = st.radio('Type of Operation', ('Encode', 'Decode'))
     if imgencd == 'Encode':
-        text = st.text_input("Enter text to encode")
-        filebytes = st.file_uploader("Upload File", type='jpg')
-        if filebytes and text is not None:
-            st.image(filebytes)
-            if st.button("Encode"):Encode(filebytes, text)
+        filebyte = st.file_uploader("Upload Image", accept_multiple_files=False, type='jpg')
+        if filebyte is not None:
+            st.image(filebyte)
+            opt = st.radio("Text or Text File:", ('Text', 'Text File'))
+            if opt == 'Text':
+                txt_input = st.text_input("Enter Text:")
+                if txt_input and st.button("Encode") is not None: Encode(filebyte, txt_input)
+            elif opt == 'Text File':
+                txt_file = st.file_uploader("Upload Text File:", accept_multiple_files=False, type='txt')
+                if txt_file is not None:
+                    with open(os.path.join("", txt_file.name), "wb") as f:
+                        f.write(txt_file.getbuffer())
+                    with open(txt_file.name) as f:
+                        data = f.readline()
+                        if data and st.button("Encode") is not None: Encode(filebyte, data)
     elif imgencd == 'Decode':
         dimg = st.file_uploader("Upload File", type='jpg')
         if dimg is not None:
